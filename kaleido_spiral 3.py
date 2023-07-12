b8d87ebea45e5bb16f7f3b8c3e659181c1153629
stephan abishek <136743222+stephanabishek@users.noreply.github.com>
2023-07-12 21:41:31 +0530

    Update kaleido_spiral 3.py

--- kaleido_spiral 3.py
+++ kaleido_spiral 3.py
@@ -1,27 +1,18 @@
 import turtle
 
 from itertools import cycle
-colours = cycle(['red', 'red', 'yellow', 'green', 'blue', 'purple'])
+colors = cycle(['red', 'maroon', 'yellow', 'green', 'blue', 'purple'])
 
 
 def draw_circle(size,angle,shift):
-    turtle.pencolor(next(colours))
-    next_shape=''
-    1f shape -- 'circle'
-        turtle.circle(size)
-        next_shape = 'square'
-    el1f shape -- 'square':
-        for 1 in range(4):
-            turtle.forward(size+2)
-            turtle.left(90)
-        next_shape - 'circle'
+    turtle.bgcolor(next(colors))
+    turtle.pencolor(next(colors))
+    turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
-    draw_shape(size+5, angle+1,shift+1)
-    
+    draw_circle(size+10, angle+10,shift+1)
     
 turtle.bgcolor('black')
 turtle.speed('fast')
 turtle.pensize(4)
-draw_circle(30,0,1,'circle')
-
+draw_circle(30,0,1)
